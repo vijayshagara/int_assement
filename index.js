7ee302278088315aa62fe1b9f5b4307d6a01a165
vijayshagara <vijayshagara1221@gmail.com>
2024-04-20 10:28:21 +0530

    new code push

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
-function printPatternInRow(n) {
+function generaterow(n) {
+  let grid = "";
+
   for (let i = 0; i < n; i++) {
-    if (i % 4 === 0 || i % 4 === 1) {
-      console.log("* * * *");
+    if (i < 2) {
+      grid += "* ".repeat(n).trim() + "\n";
+    } else if (i < 4) {
+      grid += "# ".repeat(n).trim() + "\n";
     } else {
-      console.log("# # # #");
+      grid += "* ".repeat(n).trim() + "\n";
     }
   }
+
+  console.log(grid)
 }
 
-// when n = 4
-console.log("------------- n = 4 ------------------")
-printPatternInRow(4);
-// when n = 5
-console.log("------------- n = 5 ------------------")
-printPatternInRow(5);
+console.log('------------ n = 4 ---------')
+generaterow(4);
 
-console.log('---------------------------------------------------------');
+console.log('------------ n = 5 ---------')
+generaterow(5);
 
-function printPatternInColoum(n) {
-  for (let i = 0; i < 4; i++) {
-    let row = '';
+console.log('------------ n = 6 ---------')
+generaterow(6);
 
+
+
+function generatecolumn(n) {
+  let grid = "";
+  for (let i = 0; i < n; i++) {
+    let row = "";
     for (let j = 0; j < n; j++) {
-      if (j % 4 === 0 || j % 4 === 1) {
-        row += '* ';
+      if (j < 2) {
+        row += "* "; 
+      } else if (j < 4) {
+        row += "# "; 
       } else {
-        row += '# ';
+        row += "* ";
       }
     }
-
-    console.log(row);
+    grid += row + "\n";
   }
+
+  console.log(grid);
 }
 
-// when n = 4
-console.log("------------- n = 4 ------------------")
-printPatternInColoum(4);
-// when n = 5
-console.log("------------- n = 5 ------------------")
-printPatternInColoum(5);
+console.log('------------ n = 4 ---------')
+generatecolumn(4); 
 
+console.log('------------ n = 5 ---------')
+generatecolumn(5);
 
-
-
-
+console.log('------------ n = 6 ---------')
+generatecolumn(6);
